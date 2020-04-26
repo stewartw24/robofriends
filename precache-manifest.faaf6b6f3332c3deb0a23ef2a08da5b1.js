8a63575a26640cf27ad48431f1ed4e86f27227a9
William <stewart-w24@hotmail.co.uk>
2020-04-26 08:17:57 +0100

    Updates

---
+++ precache-manifest.faaf6b6f3332c3deb0a23ef2a08da5b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0c9bb00378442593e577ce2e92f0c9",
+    "url": "//github.com/stewartw24/robofriends/index.html"
+  },
+  {
+    "revision": "343b1928e2a53aebe456",
+    "url": "//github.com/stewartw24/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "218d0d51e3036b5f1b22",
+    "url": "//github.com/stewartw24/robofriends/static/css/main.a889f2a6.chunk.css"
+  },
+  {
+    "revision": "343b1928e2a53aebe456",
+    "url": "//github.com/stewartw24/robofriends/static/js/2.2b73e027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//github.com/stewartw24/robofriends/static/js/2.2b73e027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "218d0d51e3036b5f1b22",
+    "url": "//github.com/stewartw24/robofriends/static/js/main.346f8b22.chunk.js"
+  },
+  {
+    "revision": "584cccc18517b59bc0c0",
+    "url": "//github.com/stewartw24/robofriends/static/js/runtime-main.0f676a2b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "//github.com/stewartw24/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
